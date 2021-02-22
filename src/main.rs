8a781792580fc0a7bdb110d4b28f95729093b38a
cathiecode <cathiecode@gmail.com>
2021-02-22 09:26:39 +0900

    initial

---
+++ src/main.rs
@@ -0,0 +1,80 @@
+/*
+view! {
+    (Column on_press=>Message::IncrementSpeed {
+
+    })
+}
+ */
+
+use iced::{button, Align, Button, Column, Element, Sandbox, Settings, Text};
+
+pub fn main() -> iced::Result {
+    Counter::run(Settings::default())
+}
+
+#[derive(Default)]
+struct Counter {
+    value: i32,
+    increment_button: button::State,
+    decrement_button: button::State,
+}
+
+#[derive(Debug, Clone, Copy)]
+enum Message {
+    IncrementPressed,
+    DecrementPressed,
+}
+
+impl Sandbox for Counter {
+    type Message = Message;
+
+    fn new() -> Self {
+        Self::default()
+    }
+
+    fn title(&self) -> String {
+        String::from("Counter - Iced")
+    }
+
+    fn update(&mut self, message: Message) {
+        match message {
+            Message::IncrementPressed => {
+                self.value += 1;
+            }
+            Message::DecrementPressed => {
+                self.value -= 1;
+            }
+        }
+    }
+
+    fn view(&mut self) -> Element<Message> {
+        macro_rules! view {
+            (($ty:expr $(($($new_prop:expr),*))? $($prop_id:ident={$prop:expr})* $({$($children:tt)*})?)) => ({
+              let component = $ty($($($new_prop,)*)?);
+              $(let component = component.$prop_id($prop);)*
+              $($(let component = component.push(view!($children));)*)?
+              component
+            })
+        }
+        /*Column::new()
+            .padding(20)
+            .align_items(Align::Center)
+            .push(
+                Button::new(&mut self.increment_button, Text::new("Increment"))
+                    .on_press(Message::IncrementPressed),
+            )
+            .push(Text::new(self.value.to_string()).size(50))
+            .push(
+                Button::new(&mut self.decrement_button, Text::new("Decrement"))
+                    .on_press(Message::DecrementPressed),
+            )
+            .into()*/
+        view!(
+            (Column::new padding={20} align_items={Align::Center} {
+                (Button::new(&mut self.increment_button, Text::new("Increment")) on_press={Message::IncrementPressed})
+                (Text::new(self.value.to_string()) size={50})
+                (Button::new(&mut self.decrement_button, Text::new("Decrement")) on_press={Message::DecrementPressed})
+            })
+        ).into()
+    }
+}
